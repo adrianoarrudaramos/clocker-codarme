59a8c4831d499ba999aa2373c790b855eb53758b
Adriano Arruda Ramos <adrianoarrudaramos@gmail.com>
2021-04-10 15:13:17 -0300

    feat: login and signup with firebase auth

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useFormik } from 'formik'
 import * as yup from 'yup'
 
@@ -10,31 +11,17 @@ import {
   FormControl, 
   FormLabel, 
   FormHelperText,
-  InputGroup,
-  InputLeftAddon
 } from '@chakra-ui/react'
 
 import { Logo } from './../components'
-//import firebase from './../config/firebase'
+import firebase from './../config/firebase'
 
 const validationSchema = yup.object().shape({
   email: yup.string().email('E-mail inválido').required('Preenchimento obrigatório'),
   password: yup.string().required('Preenchimento obrigatório'),
-  username: yup.string().required('Preenchimento obrigatório')
 })
 
 export default function Home() {
-
-  //dá pra fazer destructuring com formik
-  // const formik = useFormik({
-  //   onSubmit: () => {},
-  //   validationSchema,
-  //   initialValues: {
-  //     email: '',
-  //     username: '',
-  //     password: ''
-  //   }
-  // })
   const { 
     values, 
     errors, 
@@ -44,8 +31,13 @@ export default function Home() {
     handleSubmit,
     isSubmitting
   } = useFormik({
-    onSubmit: (values, form) => {
-      console.log(values)
+    onSubmit: async (values, form) => {
+      try {
+        const user = await firebase.auth().signInWithEmailAndPassword(values.email, values.password)
+        console.log(user)
+      } catch (error) {
+        console.log('ERROR: ', error)
+      }
     },
     validationSchema,
     initialValues: {
@@ -74,20 +66,12 @@ export default function Home() {
           {touched.password && <FormHelperText textColor='#e74c3c'>{errors.password}</FormHelperText>}
         </FormControl>
 
-        <FormControl id="username" p={4} isRequired>
-          <InputGroup size="lg" >
-            <InputLeftAddon children="clocker.work/" />
-            <Input type="username" value={values.username} onChange={handleChange} onBlur={handleBlur} />
-            {touched.username && <FormHelperText textColor='#e74c3c'>{errors.username}</FormHelperText>}
-          </InputGroup>
-        </FormControl>
-
         <Box p={4}>
           <Button width="100%" colorScheme="blue" onClick={handleSubmit} isLoading={isSubmitting}>Entrar</Button>
         </Box>
-
       </Box>
 
+    <Link href="/signup">Ainda não tem uma conta? Cadastre-se!</Link>
     </Container>
   )
 }
