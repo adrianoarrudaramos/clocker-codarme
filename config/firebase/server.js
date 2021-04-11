c1f9ec69c5e767ee8929dd37e05e7af1ca1f69c5
Adriano Arruda Ramos <adrianoarrudaramos@gmail.com>
2021-04-11 20:48:07 -0300

    feat: create users profile

--- config/firebase/server.js
+++ config/firebase/server.js
@@ -1,20 +1,19 @@
-import firebaseServer from 'firebase-firebaseServer'
-
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
-// };
+import firebaseServer from 'firebase-admin'
 
 const app = firebaseServer.apps.length 
   ? firebaseServer.app() 
   : firebaseServer.initializeApp({
       credential: firebaseServer.credential.cert({
-        type: process.env.TYPE
+        type: "service_account",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        client_x509_cert_url: process.env.CLIENT_CERT
       })
   });
 
